8aaffe3bf30d0448a230770118220ca79b22e0ad
KOVU9529 <bapgiordano@gmail.com>
2022-06-07 15:47:11 +0200

    Millestone-1-completed

---
+++ millestone-1/js/script.js
@@ -0,0 +1,19 @@
+/*MILLETONE-1*/
+
+/*CREAZIONE CICLO*/
+
+for (let i = 1; i <= 100; i++) {
+
+    let fizzOrBuzz;
+
+    /*IMPOSTO CONDIZIONI*/
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        fizzOrBuzz='FizzBuzz';
+    } else if (i % 5 === 0) {
+        fizzOrBuzz='Buzz';
+    } else if (i % 3 === 0) {
+        fizzOrBuzz='Fizz';
+    }
+    console.log( i + ' ' + fizzOrBuzz)
+}
